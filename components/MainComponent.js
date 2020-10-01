517f82ae32255723afe62624c3050fe80bd042e4
Namanpreet Kaur <namangurnaz@gmail.com>
2020-10-01 17:06:11 +0530

    Navigation Part Two

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
-import { DISHES } from '../shared/dishes';
+import Home from './HomeComponent';
 import Dishdetail from './DishdetailComponent';
-import { View, Platform } from 'react-native';
+import { View, Platform, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack'
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
 const MenuNavigator = createStackNavigator();
+
+const HomeNavigator = createStackNavigator();
+
+const MainNavigator = createDrawerNavigator();
 
 function MenuNavigatorScreen() {
   return(
@@ -39,16 +44,64 @@ function MenuNavigatorScreen() {
   );
 }
 
+function HomeNavigatorScreen() {
+  return(
+    <HomeNavigator.Navigator
+      initialRouteName='Home'
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#512DA8"
+        },
+        
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          color: "#fff"            
+          }
+        }}
+    >
+
+      <HomeNavigator.Screen 
+        name = "Home"
+        component = {Home}
+        options = {{ headerTitle: "Home"}}
+      />
+
+    </HomeNavigator.Navigator>
+  );
+}
+
+function MainNavigatorScreen() {
+  return(
+    <MainNavigator.Navigator drawerStyle= {{backgroundColor : '#D1C4E9'}} >
+
+      <MainNavigator.Screen 
+        name = "Home"
+        component = {HomeNavigatorScreen}
+        options = {{title: "Home"}}
+      />
+
+      <MainNavigator.Screen 
+        name = "Menu"
+        component = {MenuNavigatorScreen}
+        options = {{title: "Menu"}}
+      />
+
+    </MainNavigator.Navigator>
+  );
+}
+
 
 class Main extends Component {
 
   render() {
 
 
-    return (	    
-      <NavigationContainer>
-        <MenuNavigatorScreen />           
-      </NavigationContainer>
+    return (
+      <View style={{flex:1,paddingTop:Platform.OS==='ios'?0:StatusBar.currentHeight}} >
+        <NavigationContainer>
+          <MainNavigatorScreen />           
+        </NavigationContainer>
+      </View>
     );	    
   }	  
 }
