06737ff10d3282b8256c5e7e355535ae87010f01
Namanpreet Kaur <namangurnaz@gmail.com>
2020-10-07 22:55:04 +0530

    Gestures Part 2

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -26,6 +26,8 @@ function RenderDish(props) {
 
     const dish = props.dish;
 
+    handleViewRef = ref => this.view = ref;
+
     const recognizeDrag = ({ moveX, moveY, dx, dy}) => {
         if (dx < -200)
             return true;
@@ -36,6 +38,10 @@ function RenderDish(props) {
     const panResponder = PanResponder.create({
         onStartShouldSetPanResponder: (e, gestureState) => {
             return true;
+        },
+        onPanResponderGrant: () => {
+            this.view.rubberBand(1000)
+            .then(endState => console.log(endState.finished ? 'finished' : 'cancelled'));
         },
         onPanResponderEnd: (e, gestureState) => {
             if(recognizeDrag(gestureState))
@@ -63,6 +69,7 @@ function RenderDish(props) {
         if (dish != null) {
             return(
                 <Animatable.View animation = 'fadeInDown' duration = {2000} delay = {1000}
+                    ref= {this.handleViewRef}
                     {...panResponder.panHandlers}>
 
                     <Card
